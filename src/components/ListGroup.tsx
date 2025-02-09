ef1c6c4b9f4519c0b068c26a7915dbc5f205bc51
teli203 <teli203@aol.com>
2025-02-09 01:20:12 -0600

    Replace console log form clicked to the actual item and each item shows in console

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,7 +9,7 @@ function ListGroup() {
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log('Clicked')}
+            onClick={() => console.log(item)}
           >
             {item}
           </li>
