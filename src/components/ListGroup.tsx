a79ea18229ff8472da5ccd8cfd08f9971b6d191b
teli203 <teli203@aol.com>
2025-02-09 00:54:42 -0600

    Add bootstrap class to the li item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,7 +6,7 @@ function ListGroup() {
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item"key={item}>{item}</li>
         ))}
       </ul>
     </>
