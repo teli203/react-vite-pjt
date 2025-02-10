ff2630b8b854390512c8f9dd4fcab1e37955f04e
teli203 <teli203@aol.com>
2025-02-10 00:52:22 -0600

    Add set select for each item to be active once clicked

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,16 @@
-const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+import { useState } from "react";
 
 function ListGroup() {
+  let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log(item)}
-          >
+        {items.map((item, index) => (
+          <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={() => { setSelectedIndex(index);}}>
             {item}
           </li>
         ))}
